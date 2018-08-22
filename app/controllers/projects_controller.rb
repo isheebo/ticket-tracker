67aa7e03def057abdd322237b3311cfeaa1befff
Kanyesigye Edgar <kedgar751@gmail.com>
2018-08-22 04:30:28 +0300

    projects controller actions - show, new and create
    - ensure test is passing

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,4 +5,26 @@ class ProjectsController < ApplicationController
   def new
     @project = Project.new
   end
+
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      # flash[:notice] = "Project has been successfully created"
+      redirect_to @project, notice: "Project has been created"
+    else
+      # if the validations on the model fail, run the
+      # code which is here
+    end
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :description)
+  end
 end
