1026317949759f56bad0c5b2c1e01add17d9fd8e
Kanyesigye Edgar <kedgar751@gmail.com>
2018-08-25 08:57:41 +0300

    catch exceptions in a human friendly way
    - refine factories by giving them dynamic attributes
    - use a before_action to dry the code
    - handle errors due to wrong non-existent IDs

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe ProjectsController, type: :controller do
+  it "handles missing projects correctly" do
+    get :show, params: { id: "not-here" }
 
+    expect(response).to redirect_to(projects_path)
+
+    message = "The project you are looking for couldn't be found"
+    expect(flash[:alert]).to eq message
+  end
 end
--- spec/features/view_projects_spec.rb
+++ spec/features/view_projects_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.feature "Users can view projects", type: :feature do
   scenario "with project details" do
-    project = FactoryBot.create(:project, name: "Sublime Text 3")
+    project = FactoryBot.create(:project, name: "Entebbe Express Highway")
 
     visit "/"
-    click_link "Sublime Text 3"
+    click_link "Entebbe Express Highway"
     expect(page.current_url).to eq project_url(project)
   end
 end
